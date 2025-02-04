73e74668cd26d99f1139e9f314b1e51f38f2b955
Yassine El-jdid <eljdidyassine@gmail.com>
2025-02-04 20:18:17 +0100

    from pandas to lists

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import pandas as pd
 
-file_path = ''
-df = pd.read_csv(file_path)
+file_path = 'marketing_titles.xlsx'
+data = pd.read_excel(file_path, sheet_name=None)
+
+functions = [data['function'].columns[0]] + data['function'].iloc[:,0].tolist()
+seniorities = [data['seniority'].columns[0]] + data['seniority'].iloc[:,0].tolist()
+
+job_titles = []
+for func in functions:
+    for seniority in seniorities:
+        if func and seniority:
+            job_titles.append(f'{func.strip()} {seniority.strip()}')
+
+output = pd.DataFrame(job_titles, columns=['job_title'])
+output.to_csv('job_titles.csv', index=False)
